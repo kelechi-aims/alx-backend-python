c3583f82c1b1068ecf467cd2c5d645a6e3dea7a7
kelechi-aims <kelechi@aims.edu.gh>
2024-01-11 22:06:54 +0100

    task-1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+"""
+Write a type-annotated function concat that takes a string str1 and
+a string str2 as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Parameters:
+    - str1 (str): The first string.
+    - str2 (str): The second string.
+
+    Returns:
+    str: The concatenated string of str1 and str2.
+    """
+    return str1 + str2
