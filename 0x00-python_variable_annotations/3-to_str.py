9cef5c4d150c33b32cf781d1d018425f374f90b4
kelechi-aims <kelechi@aims.edu.gh>
2024-01-11 22:33:29 +0100

    task-3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function to_str that takes a float n as
+argument and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the str representation of a floating-point number.
+
+    Parameters:
+    - n (float): The input floating-point number.
+
+    Returns:
+    str: The str representation of the input number.
+    """
+    return str(n)
