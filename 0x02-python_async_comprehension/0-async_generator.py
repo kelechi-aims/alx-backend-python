fdd65a90be52509fcea409e82636956355235997
kelechi-aims <kelechi@aims.edu.gh>
2024-01-16 16:13:49 +0100

    task_0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,6 +6,13 @@ import random
 
 
 async def async_generator() -> Generator[float, None, None]:
+    """
+    An asynchronous generator coroutine that yields random
+    float values between 0 and 10.
+
+    Yields:
+    - float: A random float between 0 and 10.
+    """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
