c09524b4e9e02e26818ff37c1e1bd22cc208cfce
kelechi-aims <kelechi@aims.edu.gh>
2024-01-16 16:11:42 +0100

    task_0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Write a coroutine called async_generator that takes no arguments. """
+from typing import Generator
+import asyncio
+import random
+
+
+async def async_generator() -> Generator[float, None, None]:
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
